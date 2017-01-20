633ca99e3b9b5d16e0afe336d7ef2222ce5e8ea6
atti98 <Jungdeokho@gmail.com>
2017-01-20 12:13:00 +0900

    문제추가 ( 3번 )
    The prime factors of 13195 are 5, 7, 13 and 29.
    
    What is the largest prime factor of the number 600851475143 ?

--- eulerQuest_3.c
+++ eulerQuest_3.c
@@ -2,10 +2,9 @@
 int main () {
 	long long int x=600851475143;
 	int y=3;
-	while (!(x/y==1 && x%y==0)){
-		while (x%y==0){
-			x=x/y;
-		}
+	/* factor will be % answer = 0, prime factor will be odd number except 2 */
+	while (x!=y) {
+    		if(x%y==0) x=x/y;
 		y+=2;
 	}
 	printf("%d\n", y);
